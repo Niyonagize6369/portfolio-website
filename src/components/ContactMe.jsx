d2443e7797d55d44fd9d02179df7c86f6aa164a4
niyonagize6369 <niyonagizerachel10@gmail.com>
2025-01-21 16:51:29 +0530

    portfolio pushed

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -42,7 +42,7 @@ const ContactMe = () => {
             </div>
             <div className="mb-4">
               <FaMapMarkedAlt className="inline-block text-green-400 mr-2"></FaMapMarkedAlt>
-              <span>City,Province,Country</span>
+              <span>Kigali,Kigali,Rwanda</span>
             </div>
           </div>
           <div className="flex-1 w-full">
